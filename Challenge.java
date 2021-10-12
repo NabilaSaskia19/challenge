dc17dd67a914d215d0ddc7512980b69ebe2a2e78
NabilaSaskia19 <91514373+NabilaSaskia19@users.noreply.github.com>
2021-10-12 13:43:40 +0700

    first commit

---
+++ Challenge.java
@@ -0,0 +1,18 @@
+public class Challenge {
+    public static void main (String[] args){
+    String firstName = "Nabila ";
+    String lastName = "Saskia ";
+    int age = 21;
+    String address = "jl.karya Cilincing ";
+    String phoneNumber = "081808886587 ";
+    String hobi = "makan ";
+    String makes = "sate padang ";
+    System.out.println("first name : " + firstName);
+    System.out.println("last name : " + lastName);
+    System.out.println("Age : " + age);
+    System.out.println("address : " + address);
+    System.out.println("phone number : " + phoneNumber);
+    System.out.println("hobi : " + hobi);
+    System.out.println("makanan kesukaan : " + makes);
+    }
+}
